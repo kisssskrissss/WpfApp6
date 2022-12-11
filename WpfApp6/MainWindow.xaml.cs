79c792055b2694352df0b7737ca2660f7301a7d0
krist <krist@LAPTOP-HIQLHLL6>
2022-12-11 21:59:54 +0500

    Добавьте файлы проекта.

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    enum Precipitation
+    {
+        sunny,
+        cloudy,
+        rain,
+        snow
+    }
+    class WeatherControl: DependencyObject
+    {
+        private Precipitation precipitation;
+        private string wind_direction;
+        private int wind_speed;
+
+        public string WindDirection { get; set; }
+        public int WindSpeed { get; set; }
+        public WeatherControl(string windir, int winsp, Precipitation precipitation)
+        {
+            this.WindDirection = windir;
+            this.WindSpeed = winsp;
+            this.precipitation = precipitation;
+        }
+        public static readonly DependencyProperty TempProperty;
+        public int Temp
+        {
+            get => (int)GetValue(TempProperty);
+                set => SetValue(TempProperty, value);
+        }
+        static WeatherControl()
+        {
+            TempProperty = DependencyProperty.Register(
+                nameof(Temp),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsRender,
+                    null,
+                    new CoerceValueCallback(CoerceTemp)),
+                new ValidateValueCallback(ValidateTemp));
+
+        }
+
+        private static bool ValidateTemp(object value)
+        {
+            int v = (int)value;
+            if (v>=-50 && v <= 50)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static object CoerceTemp(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50 && v <= 50)
+            {
+                return v;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+    
+}
